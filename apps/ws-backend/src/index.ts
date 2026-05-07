1738ce1009b30421a2d0b9e189507552a534ea91
saranshmehta33 <saranshmehta3333@gmail.com>
2026-05-07 10:58:01 +0530

    feat: first commit initialized the http and ws servers

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,12 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port: 8090});
+
+wss.on('connection', function connection(ws) {
+
+    ws.on('message', function message(data) {
+        ws.send('pong');
+    });
+
+    
+});
