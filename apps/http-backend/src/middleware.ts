6bc3b4b224f766a2bc06a8c97fefefa1f6a24ae6
saranshmehta33 <saranshmehta3333@gmail.com>
2026-05-07 12:33:56 +0530

    feat: added signup, signin, room routes with jwt

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config.js";
+
+export function middleware(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers["authorization"] ?? "";
+
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+
+    if(decoded) {
+        req.userId = decoded.userId;
+    } else {
+        res.status(403).json({
+            message: "Unauthorized"
+        })
+    }
+}
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,12 +1,27 @@
 import { WebSocketServer } from "ws";
+import jwt, { decode, JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const wss = new WebSocketServer({port: 8090});
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
+    const url = request.url; //ws://localhost:3000?token=123123
+    // ["ws://localhost:3000", "token=123123"]
+    if (!url) {
+        return;
+    }
+
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') ?? "";
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(!decoded || !(decoded as JwtPayload).userId) {
+        ws.close();
+        return;
+    }
 
     ws.on('message', function message(data) {
         ws.send('pong');
     });
 
-    
 });
